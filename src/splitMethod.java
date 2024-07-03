99303a4eecd39e05552986711fc1d1cbb9b5b5e8
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-07-03 11:44:10 -0400

    did more problems

--- src/splitMethod.java
+++ src/splitMethod.java
@@ -1,6 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class splitMethod {
     public static void main(String[] args) {
         String vowels = "a::b::c::d::e";
-        
+        String[] result = vowels.split("::");
+        System.out.println("result = " + Arrays.toString(result));
     }
 }
